aa389017105c9cd483e3458c049e1fec1502d751
Fabien Barbaud <fabien.barbaud@timeonegroup.com>
2021-03-09 22:11:50 +0100

    feat: docker + linter

---
+++ docker/start_app.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+pip install -e .[app]
+python app.py
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name="flaskex",
+    extras_require={
+        "app": ["Flask", "WTForms", "SQLAlchemy", "bcrypt", "requests", "flask-heroku", "gunicorn"],
+        "linter": ["flake8"],
+        "test": ["pytest"],
+    },
+)
