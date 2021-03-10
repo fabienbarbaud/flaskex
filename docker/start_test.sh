7ffd849bd2a3cb22dfd4d8bc3904dd2a2a3abf13
Fabien Barbaud <fabien.barbaud@timeonegroup.com>
2021-03-10 17:00:32 +0100

    feat: ajoute selenium

--- docker/start_test.sh
+++ docker/start_test.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+wait-for-url() {
+    echo "Testing $1"
+    timeout -s TERM 45 bash -c \
+    'while [[ "$(curl -s -o /dev/null -L -w ''%{http_code}'' ${0})" != "200" ]];\
+    do echo "Waiting for ${0}" && sleep 2;\
+    done' ${1}
+    echo "OK!"
+    curl -I $1
+}
+
 pip install -e .[test]
+
+wait-for-url http://app:5000
 pytest
